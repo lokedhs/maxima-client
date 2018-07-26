6d5be72a2e8f6c2cacaa4afceefd71eb8330a1de
Elias Martenson <lokedhs@gmail.com>
2018-07-26 19:14:57 +0800

    Ability to exit the matrix editor

--- src/matrix-edit.lisp
+++ src/matrix-edit.lisp
@@ -39,15 +39,20 @@
 
 (clim:define-command-table edit-matrix-commands)
 
-(defun make-matrix-edit-field (stream value)
-  (let* ((edit-stream nil)
+(defun make-matrix-edit-field (stream value selected)
+  (let* ((margin-width 5)
+         (edit-stream nil)
          (rec (clim:updating-output (stream :unique-id (list 'matrix-edit
                                                              (matrix-edit-value/row value)
                                                              (matrix-edit-value/col value))
                                             :id-test #'equal)
                 (clim:with-output-as-presentation (stream value 'matrix-edit-value :allow-sensitive-inferiors nil)
                   (let ((expr-rec (make-rendered-output-record (stream)
-                               (render-maxima-expression stream (matrix-edit-value/orig value)))))
+                                    (render-maxima-expression stream (matrix-edit-value/orig value)))))
+                    (dimension-bind (expr-rec :x x :y y :right right :bottom bottom)
+                      (clim:draw-rectangle* stream x y (+ right (* margin-width 2)) (+ bottom (* margin-width 2))
+                                            :ink (if selected clim:+lightgrey+ clim:+white+)))
+                    (move-rec expr-rec margin-width margin-width)
                     (clim:stream-add-output-record stream expr-rec))))))
     (setf (matrix-edit-value/record value) rec)
     (setf (matrix-edit-value/edit-stream value) edit-stream)))
@@ -59,59 +64,84 @@
       (error "Variable does not contain a matrix"))
     (let* ((stream *standard-output*)
            (rec nil)
-           (values nil))
+           (values nil)
+           (curr-row 0)
+           (curr-col 0)
+           (active t)
+           (old-cursor-pos (multiple-value-list (clim:stream-cursor-position stream))))
       (setq rec (clim:updating-output (stream)
-                  (clim:formatting-table (stream)
-                    (loop
-                      for matrix-row in (cdr matrix-content)
-                      for row from 0
-                      for row-values = (let ((col-values nil))
-                                         (assert (eq (caar matrix-row) 'maxima::mlist))
-                                         (clim:formatting-row (stream)
-                                           (loop
-                                             for matrix-col in (cdr matrix-row)
-                                             for col from 0
-                                             for value = (make-instance 'matrix-edit-value
-                                                                        :orig matrix-col
-                                                                        :row row
-                                                                        :col col)
-                                             do (clim:formatting-cell (stream)
-                                                  (make-matrix-edit-field stream value))
-                                             collect value into result
-                                             finally (setq col-values result)))
-                                         col-values)
-                      collect row-values into result
-                      finally (setq values result)))))
-      (let ((curr-row 0)
-            (curr-col 0))
-        (labels ((p ()
-                   (log:info "pos: ~s,~s" curr-row curr-col))
-                 (constrain-col (new-col)
-                   (let ((row (nth curr-row values)))
-                     (setf curr-col (max (min new-col (1- (length row))) 0))))
-                 (move-x (delta)
-                   (constrain-col (+ curr-col delta))
-                   (p))
-                 (move-y (delta)
-                   (setf curr-row (max (min (+ curr-row delta) (1- (length values))) 0))
-                   (constrain-col curr-col)
-                   (p)))
-          (loop
-            for gesture = (clim:with-input-context ('matrix-edit-value :override t)
-                              (object type)
-                              (clim:read-gesture :stream stream)
-                            (matrix-edit-value
-                             (log:info "value clicked = ~s" object)))
-            when (typep gesture 'clim:key-press-event)
-              do (let ((event-name (clim:keyboard-event-key-name gesture)))
-                   (if (gesture-modifier-p gesture :control)
-                       (case event-name
-                         (:|p| (move-y -1))
-                         (:|n| (move-y 1))
-                         (:|b| (move-x -1))
-                         (:|f| (move-x 1)))
-                       (case event-name
-                         (:up (move-y -1))
-                         (:down (move-y 1))
-                         (:left (move-x -1))
-                         (:right (move-x 1)))))))))))
+                  (when active
+                    (clim:formatting-table (stream)
+                      (clim:formatting-row (stream)
+                        (clim:formatting-cell (stream)
+                          (format stream " "))
+                        (loop
+                          for i from 1 to (reduce #'max (mapcar (lambda (v)
+                                                                  (length (cdr v)))
+                                                                (cdr matrix-content)))
+                          do (clim:formatting-cell (stream :align-x :center :align-y :center)
+                               (clim:with-text-style (stream (clim:make-text-style :sans-serif :bold nil))
+                                 (format stream "~a" i)))))
+                      (loop
+                        for matrix-row in (cdr matrix-content)
+                        for row from 0
+                        for row-values = (let ((col-values nil))
+                                           (assert (eq (caar matrix-row) 'maxima::mlist))
+                                           (clim:formatting-row (stream)
+                                             (clim:formatting-cell (stream :align-x :center :align-y :center)
+                                               (clim:with-text-style (stream (clim:make-text-style :sans-serif :bold nil))
+                                                 (format stream "~a" (1+ row))))
+                                             (loop
+                                               for matrix-col in (cdr matrix-row)
+                                               for col from 0
+                                               for value = (make-instance 'matrix-edit-value
+                                                                          :orig matrix-col
+                                                                          :row row
+                                                                          :col col)
+                                               do (clim:formatting-cell (stream)
+                                                    (make-matrix-edit-field stream value (and (= curr-row row)
+                                                                                              (= curr-col col))))
+                                               collect value into result
+                                               finally (setq col-values result)))
+                                           col-values)
+                        collect row-values into result
+                        finally (setq values result))))))
+      (labels ((redraw ()
+                 (log:info "pos: ~s,~s" curr-row curr-col)
+                 (clim:redisplay rec stream))
+               (constrain-col (new-col)
+                 (let ((row (nth curr-row values)))
+                   (setf curr-col (max (min new-col (1- (length row))) 0))))
+               (move-x (delta)
+                 (constrain-col (+ curr-col delta))
+                 (redraw))
+               (move-y (delta)
+                 (setf curr-row (max (min (+ curr-row delta) (1- (length values))) 0))
+                 (constrain-col curr-col)
+                 (redraw)))
+        (loop
+          named matrix-edit-main-loop
+          for gesture = (clim:with-input-context ('matrix-edit-value :override t)
+                            (object type)
+                            (clim:read-gesture :stream stream)
+                          (matrix-edit-value
+                           (log:info "value clicked = ~s" object)))
+          when (typep gesture 'clim:key-press-event)
+            do (let ((event-name (clim:keyboard-event-key-name gesture)))
+                 (log:info "event name: ~s" event-name)
+                 (if (gesture-modifier-p gesture :control)
+                     (case event-name
+                       (:|p| (move-y -1))
+                       (:|n| (move-y 1))
+                       (:|b| (move-x -1))
+                       (:|f| (move-x 1)))
+                     (case event-name
+                       (:up (move-y -1))
+                       (:down (move-y 1))
+                       (:left (move-x -1))
+                       (:right (move-x 1))
+                       (:escape
+                        (setq active nil)
+                        (redraw)
+                        (setf (clim:stream-cursor-position stream) (apply #'values old-cursor-pos))
+                        (return-from matrix-edit-main-loop nil))))))))))
