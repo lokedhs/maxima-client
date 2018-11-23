f1abbd3ea34d2d96a2578d4d20d945ba7d997629
Elias Mårtenson <lokedhs@gmail.com>
2018-11-23 18:57:27 +0800

    Some further work on parsing info documents

--- infoparser/infoparser.lisp
+++ infoparser/infoparser.lisp
@@ -6,31 +6,55 @@
                (funcall plain-string-markup-fn s)
                (list s))))
     (loop
-       with length = (length string)
-       with start = 0
-       while (< start length)
-       append (multiple-value-bind (match-start match-end reg-starts reg-ends)
-                  (cl-ppcre:scan regexp string :start start)
-                (if match-start
-                    ;; Some highlighted text was found
-                    (let* ((highlight (funcall callback reg-starts reg-ends))
-                           (old-start start))
-                      (setq start match-end)
-                      (if (> match-start old-start)
-                          ;; There is some unmatched text before the match
-                          (append (markup-string (subseq string old-start match-start))
-                                  (list highlight))
-                          ;; ELSE: The match is at the beginning of the string
-                          (list highlight)))
-                    ;; ELSE: No match, copy the last part of the text and finish the loop
-                    (progn
-                      (let ((old-start start))
-                        (setq start length)
-                        (markup-string (subseq string old-start)))))))))
+      with length = (length string)
+      with start = 0
+      while (< start length)
+      append (multiple-value-bind (match-start match-end reg-starts reg-ends)
+                 (cl-ppcre:scan regexp string :start start)
+               (if match-start
+                   ;; Some highlighted text was found
+                   (let* ((highlight (funcall callback reg-starts reg-ends))
+                          (old-start start))
+                     (setq start match-end)
+                     (if (> match-start old-start)
+                         ;; There is some unmatched text before the match
+                         (append (markup-string (subseq string old-start match-start))
+                                 (list highlight))
+                         ;; ELSE: The match is at the beginning of the string
+                         (list highlight)))
+                   ;; ELSE: No match, copy the last part of the text and finish the loop
+                   (progn
+                     (let ((old-start start))
+                       (setq start length)
+                       (markup-string (subseq string old-start)))))))))
 
 (defmacro markup-from-regexp (regexp string callback &optional plain-string-markup-fn &environment env)
   `(%markup-from-regexp ,(if (constantp regexp env) `(load-time-value (cl-ppcre:create-scanner ,regexp)) regexp)
                         ,string ,callback ,@(if plain-string-markup-fn (list plain-string-markup-fn))))
+
+(defun %iterate-regexps (regexp string callback)
+  (loop
+    with length = (length string)
+    with start = 0
+    while (< start length)
+    do (multiple-value-bind (match-start match-end reg-starts reg-ends)
+           (cl-ppcre:scan regexp string :start start)
+         (cond (match-start
+                (setq start match-end)
+                (let ((vec (make-array (length reg-starts) :element-type 'string :initial-element "")))
+                  (loop
+                    for start across reg-starts
+                    for end across reg-ends
+                    for i from 0
+                    do (setf (aref vec i) (subseq string start end)))
+                  (funcall callback vec)))
+               (t
+                (setq start length))))))
+
+(defmacro iterate-regexps ((sym regexp string) &body body &environment env)
+  `(%iterate-regexps ,(if (constantp regexp env) `(load-time-value (cl-ppcre:create-scanner ,regexp)) regexp)
+                     ,string
+                     (lambda (,sym) ,@body)))
 
 (defun process-menu (stream)
   (cons :menu
@@ -47,15 +71,61 @@
 
 (defun parse-paragraph (s)
   (cons :paragraph
-        (markup-from-regexp "@(code|var|mref){([^}]+)}" s
+        (markup-from-regexp "@(code|var|mref|emph|strong){([^}]+)}" s
                             (lambda (reg-starts reg-ends)
                               (let ((cmd (subseq s (aref reg-starts 0) (aref reg-ends 0)))
                                     (content (subseq s (aref reg-starts 1) (aref reg-ends 1))))
                                 (list (string-case:string-case (cmd)
                                         ("code" :code)
                                         ("var" :var)
-                                        ("mref" :mref))
-                                      content))))))
+                                        ("mref" :mref)
+                                        ("emph" :italic)
+                                        ("strong" :bold))
+                                      content)))
+                            (lambda (s)
+                              (list (cl-ppcre:regex-replace-all "@dots{}" s "…"))))))
+
+(defun process-opencatbox (stream)
+  (cons :catbox
+        (loop
+          for s = (read-line stream)
+          until (cl-ppcre:scan "^@closecatbox *$" s)
+          append (collectors:with-collector (coll)
+                   (iterate-regexps (category "@category{([^}]+)}" s)
+                     (coll (aref category 0)))
+                   (coll)))))
+
+(defun parse-node (args)
+  (labels ((nil-or-trim (s)
+             (if s
+                 (let ((trimmed (string-trim " " s)))
+                   (if (zerop (length trimmed)) nil trimmed))
+                 nil)))
+    (let* ((category-names (split-sequence:split-sequence #\, args))
+           (name (string-trim " " (nth 0 category-names)))
+           (next (nil-or-trim (nth 1 category-names)))
+           (prev (nil-or-trim (nth 2 category-names)))
+           (up (nil-or-trim (nth 3 category-names))))
+      (list :node name next prev up))))
+
+(defun %process-single-line-command (string clauses)
+  (alexandria:with-gensyms (match parts)
+    (let ((clause (car clauses)))
+      `(multiple-value-bind (,match ,parts)
+           (cl-ppcre:scan-to-strings ,(caar clause) ,string)
+         (declare (ignorable ,parts))
+         (if ,match
+             ,(if (cadar clause)
+                  `(let ((,(cadar clause) ,parts))
+                     ,@(cdr clause))
+                  `(progn ,@(cdr clause)))
+             ,@(if (cdr clauses)
+                   (list (%process-single-line-command string (cdr clauses)))
+                   nil))))))
+
+(defmacro process-single-line-command (string &body clauses)
+  (alexandria:once-only (string)
+    (%process-single-line-command string clauses)))
 
 (defun parse-file (file)
   (collectors:with-collector (info-collector)
@@ -68,15 +138,13 @@
           (loop
             for s = (read-line stream nil nil)
             while s
-            do (cond ((alexandria:starts-with-subseq "@" s)
-                      (cond ((cl-ppcre:scan "^@menu *$" s)
-                             (info-collector (process-menu stream)))
-                            ((cl-ppcre:scan "^@c +" s)
-                             ;; Comment
-                             nil)
-                            (t
-                             (log:warn "Unexpected command: ~s" s)
-                             nil)))
+            do (cond ((cl-ppcre:scan "^@[a-z]+(?: |$)" s)
+                      (process-single-line-command s
+                        (("^@menu *$") (info-collector (process-menu stream)))
+                        (("^@opencatbox *$") (info-collector (process-opencatbox stream)))
+                        (("^@node +(.*)$" args) (info-collector (parse-node (aref args 0))))
+                        (("@section +(.*[^ ]) *$" name) (info-collector (list :section (aref name 0))))
+                        (("^@c ") nil)))
                      ((cl-ppcre:scan "^ *$" s)
                       (collect-paragraph))
                      (t
