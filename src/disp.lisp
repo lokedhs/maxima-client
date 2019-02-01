36c5808c4091e84bc026a848a6ea9a1ad10ed6b3
Elias Mårtenson <lokedhs@gmail.com>
2019-02-01 18:06:26 +0800

    Improve drawing of maxima text objects

--- src/disp.lisp
+++ src/disp.lisp
@@ -30,12 +30,12 @@
 (define-render-function (render-mtext maxima::mtext stream args :inhibit-presentation t)
   (with-aligned-rendering (stream)
     (dolist (element args)
-      (etypecase element
-        (list (cond ((eq (caar element) 'maxima::text-string)
-                     (clim:with-text-family (stream :sans-serif)
-                       (render-aligned-string "~a" (coerce (cdr element) 'string))))
-                    (t
-                     (render-aligned () (render-maxima-expression stream element)))))))))
+      (cond ((and (listp element)
+                  (eq (caar element) 'maxima::text-string))
+             (clim:with-text-family (stream :sans-serif)
+               (render-aligned-string "~a" (coerce (cdr element) 'string))))
+            (t
+             (render-aligned () (render-maxima-expression stream element)))))))
 
 (wrap-function maxima::displa (expr)
   (let ((stream (maxima-io/clim-stream *standard-output*)))
